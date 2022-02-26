0661f32bcebde2690d57fac7a1ad6e63d7bf4b4a
Sonia Choudhury <soniachoudhury@C02G758FMD6M.local>
2022-02-26 20:24:30 +0000

    palette divs added

--- reactcolorpalette/src/App.tsx
+++ reactcolorpalette/src/App.tsx
@@ -3,10 +3,17 @@ import Palette from "./Palette";
 import SeedData from "./SeedColorPicker";
 
 function App() {
-  console.log("Seed4", SeedData[4].paletteName)
+  console.log("Seed4", SeedData[4].paletteName);
   return (
     <div>
-      <Palette colorData={SeedData[4]} />
+      <Palette
+        {...SeedData[4]}
+        // colorData={SeedData[4]}
+        // paletteName={SeedData[4].paletteName}
+        // id={SeedData[4].id}
+        // emoji={SeedData[4].emoji}
+        // colors={[SeedData[4].colors[0]]}
+      />
     </div>
   );
 }
--- reactcolorpalette/src/Palette.tsx
+++ reactcolorpalette/src/Palette.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 export interface PaletteProps {
-paletteName: string, 
-id: string
-emoji: string 
-colors: [ColorProps]
+  paletteName?: string;
+  id?: string;
+  emoji?: string;
+  colors?: [ColorProps];
+  colorData?: any;
+}
+export interface ColorProps {
+  name?: string;
+  color?: string;
+}
+const Palette = ({
+  paletteName,
+  id,
+  emoji,
+  colors,
+  ...props
+}: PaletteProps) => {
+  console.log("props", props);
 
-}
-export interface  ColorProps {
-    name: string
-    color: string
-}
-const Palette = (props: any) => {
-    console.log("p", props.colorData.paletteName)
-    
   return (
-    <div>
+    <div className="Palette">
+      {/* navbar */}
+      <div className="Palette-colors">
         <h1>Palette</h1>
-     <h1>{props.colorData.paletteName}</h1>
-        </div>
-  )
-}
+        <h1>name: {paletteName}</h1>
+        <h3>id: {id}</h3>
+        <h3>{colors}</h3>
+      </div>
+      {/* footer */}
+    </div>
+  );
+};
 
-export default Palette
+export default Palette;
